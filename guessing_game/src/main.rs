fd082a71527e13dd7e348fd2180989413af7f442
McLutzifer <lukasschumi@gmx.at>
2021-02-09 21:49:56 +0100

    Add code

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, guess!");
+    println!("Guess the number!");
+
+    println!("Please input your guess");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut gues)
+        .expect("Failed to read line");
+    
+    println!("You guessed: {}", guess);
 }
