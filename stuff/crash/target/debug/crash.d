cdf6f4806d2d4629b076a0b0e802f4383d144d60
McLutzifer <lukasschumi@gmx.at>
2021-02-10 20:14:03 +0100

    cargo run

---
+++ stuff/crash/target/debug/crash.d
@@ -0,0 +1,1 @@
+/home/lukas/Documents/Programming/Rust/Rust_programming_language/stuff/crash/target/debug/crash: /home/lukas/Documents/Programming/Rust/Rust_programming_language/stuff/crash/src/main.rs
---
+++ stuff/crash/target/debug/deps/crash-3cceec7fac4abf3e.d
@@ -0,0 +1,5 @@
+/home/lukas/Documents/Programming/Rust/Rust_programming_language/stuff/crash/target/debug/deps/crash-3cceec7fac4abf3e: src/main.rs
+
+/home/lukas/Documents/Programming/Rust/Rust_programming_language/stuff/crash/target/debug/deps/crash-3cceec7fac4abf3e.d: src/main.rs
+
+src/main.rs:
