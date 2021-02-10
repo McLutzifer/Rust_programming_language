ec7a2d3b4d232a06732eeabc6c3537573e40b93c
McLutzifer <lukasschumi@gmx.at>
2021-02-10 16:40:38 +0100

    add functions

--- stuff/arrays.rs
+++ stuff/arrays.rs
@@ -1,8 +1,11 @@
+fn add(x: i32, y: i32) -> i32 {
+    return x + y;
+}
+
 fn main() {
     let nums: [i32; 3] = [1, 2, 3];
     println!("{:?}", nums);
     println!("{}", nums[1]);
 
-
-    
+    println!("{}", add(42, 13));
 }
