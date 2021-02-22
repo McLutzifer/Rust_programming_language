b364521978d3b9d63e1485cf983f7b5cbbbc4dfd
McLutzifer <lukasschumi@gmx.at>
2021-02-22 19:53:27 +0100

    cargo run

--- guessing_game/target/debug/guessing_game.d
+++ guessing_game/target/debug/guessing_game.d
@@ -1,1 +1,1 @@
-/home/lukas/Documents/Programming/Rust/Rust_programming_language/guessing_game/target/debug/guessing_game: /home/lukas/Documents/Programming/Rust/Rust_programming_language/guessing_game/src/main.rs
+/home/lukas/Documents/Programming/Rust_programming_language/guessing_game/target/debug/guessing_game: /home/lukas/Documents/Programming/Rust_programming_language/guessing_game/src/main.rs
