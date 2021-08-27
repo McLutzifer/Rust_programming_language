08314db02815475658ea87f93037f3ed1ff2b261
McLutzifer <lukasschumi@gmx.at>
2021-08-27 18:53:03 +0200

    integer

--- learnXinYmuntes/WhereXisRust.rs
+++ learnXinYmuntes/WhereXisRust.rs
@@ -4,5 +4,19 @@ fn add(x: i32, y: i32) -> i32 {
 }
 
 fn main() {
+    // Nubers //
+
+    // Immutable bindings
+    let x: i32 = 1;
+
+    // Integer/float suffixes
+    let y: i32 = 13i32;
+    let f: f64 = 1.3f64;
+
+    // Type inference
+    let implicit_x = 1;
+    let implicit_f = 1.3;
+
+    // Arithmetic
     
 }
